8cdfcfcbaf06989a70bbddeda2d2a07194c6d2ad
Alexandr <alexandrlachin989@gmail.com>
2026-01-16 10:53:01 +0300

    Test unit SmsCenter

--- cabinet/public_html/APP/Module/Sms.php
+++ cabinet/public_html/APP/Module/Sms.php
@@ -7,22 +7,27 @@ use Pet\Errors\AppException;
  * @see cabinet/tests/Unit/Module/SmsTest.php
  *  Модуль тестирования SmsTest
  *  запуск в консоли
- *  composer test::filter SmsTest
+ *  composer test:filter SmsTest
  **/
 class Sms
 {
-    protected $url = 'https://smsc.ru/sys/send.php';
+    protected $url = [
+        'send' =>  'https://smsc.ru/sys/send.php',
+        'balace' => 'https://smsc.ru/sys/balance.php'
+    ];
+
+    protected $urlBalance = '';
     protected $login = 'altamedplus';
     protected $psw = 'MxiMgxThyG7C';
 
     public const FORMAT_STRING = 0;
-    public const FORMAT_JSON = 1;
+    public const FORMAT_JSON = 3;
     public const FORMAT_XML = 2;
 
     public function __construct()
     {
         if (!defined('SMS_LOGIN') || !defined('SMS_PASSWORD')) {
-            throw new AppException('Error! keys constat for smsc.ru');
+            throw new AppException('Error! keys constat in env (SMS_LOGIN, SMS_PASSWORD )for smsc.ru');
         }
 
         $this->login = SMS_LOGIN;
@@ -30,11 +35,11 @@ class Sms
     }
     public function send($phone, $text)
     {
-        return file_get_contents($this->url . "?login={$this->login}&psw={$this->psw}&phones=$phone&mes=$text");
+        return file_get_contents($this->url['send'] . "?login={$this->login}&psw={$this->psw}&phones=$phone&mes=$text");
     }
 
     public function getBalance($format = self::FORMAT_JSON)
     {
-        return file_get_contents($this->url . "?login={$this->login}&psw={$this->psw}&fmt={$format}");
+        return file_get_contents($this->url['balace'] . "?login={$this->login}&psw={$this->psw}&fmt={$format}");
     }
 }
--- cabinet/tests/Unit/Module/SmsTest.php
+++ cabinet/tests/Unit/Module/SmsTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Unit\Module;
 
 use APP\Module\Sms;
+use Exception;
+use Pet\Command\Console\Console;
 use PHPUnit\Framework\TestCase;
+
 
 class SmsTest extends TestCase
 {
@@ -12,22 +15,46 @@ class SmsTest extends TestCase
     protected function setUp(): void
     {
         parent::setUp();
-        $this->sms = new Sms();
+        try {
+            $this->sms = new Sms();
+        } catch (Exception $e) {
+            Console::log($e->getMessage(), Console::RED);
+        }
     }
 
-    public function testSendSuccess() {
+    public function testBalance()
+    {
 
-    }
+        $jsonResponse = $this->sms->getBalance(Sms::FORMAT_JSON);
 
-    public function testSendWithEmptyCredentials() {
-     
-    }
+        $this->assertNotEmpty($jsonResponse, 'Ответ от сервиса не должен быть пустым');
 
-    public function testPhoneNumberFormatting() {
-       
-    }
+        $data = json_decode($jsonResponse);
 
-    public function testTextEncoding() {
-    
+        $this->assertNotNull($data, 'Не удалось декодировать JSON ответ');
+
+        $this->assertTrue(isset($data->balance), 'В ответе отсутствует ключ "balance"');
+
+        $balance = $data->balance;
+
+        Console::log("Текущий баланс: " . $balance, Console::GREEN);
+
+        if ($balance < 0) {
+            Console::log('Баланс отрицательный: ' . $balance, Console::RED);
+            $this->assertLessThan(0, $balance, 'Баланс отрицательный');
+        } elseif ($balance == 0) {
+            Console::log('Баланс равен нулю', Console::YELLOW);
+            $this->assertEquals(0, $balance, 'Баланс должен быть равен 0');
+        } elseif ($balance < 20) {
+            Console::log('Баланс менее 20: ' . $balance, Console::YELLOW);
+            $this->assertLessThan(20, $balance, 'Баланс менее 20');
+            $this->assertGreaterThan(0, $balance, 'Баланс больше 0 но менее 20');
+        } elseif ($balance == 20) {
+            Console::log('Баланс равен 20', Console::GREEN);
+            $this->assertEquals(20, $balance, 'Баланс должен быть равен 20');
+        } else {
+            Console::log('Баланс нормальный: ' . $balance, Console::GREEN);
+            $this->assertGreaterThan(20, $balance, 'Баланс должен быть больше 20');
+        }
     }
 }
