8814b4235938fb77d8720cefce7200bd899b2585
Alexandr <alexandrlachin989@gmail.com>
2026-01-16 11:45:13 +0300

    Авторизация в макс

--- cabinet/public_html/APP/Controller/Api/Max/SubscriptionsController.php
+++ cabinet/public_html/APP/Controller/Api/Max/SubscriptionsController.php
@@ -17,8 +17,10 @@ class SubscriptionsController extends Controller
         'сброс авторизации',
         'сменить номер телефона',
     ];
+    protected Messenger $maxMessenger;
     public function index()
     {
+        $this->maxMessenger = new Messenger();
         Response::set(Response::TYPE_JSON);
         $data = attr();
         try {
@@ -42,10 +44,9 @@ class SubscriptionsController extends Controller
     {
         $contact = new Contact(['max_user_id' => $userId], isNotExistCreate:true);
         if (empty($contact->step_authorization)) {
-            $result = (new Messenger())->sendMessangeUser([
+            $result = $this->maxMessenger->sendMessangeUser([
                 'text' => 'Для получения уведолений вам требуется авторизоваться! Напишите ваш номер телефона и вам придет смс кодом и запишите его следующим сообщением.'
             ], $userId);
-            file_put_contents(__DIR__ . '/debug.txt', print_r($result, true), FILE_APPEND);
             $contact->set('step_authorization', TypeAutorization::START);
         }
     }
@@ -55,50 +56,60 @@ class SubscriptionsController extends Controller
         $userId = $data['message']['sender']['user_id'];
         $contact = new Contact(['max_user_id' => $userId]);
         $text = mb_strtolower(trim($data['message']['body']['text']));
+
         if (in_array($text, $this->command)) {
             $contact->set('step_authorization', null); // обнуляем авторизацию
+            $contact->set('phone', null);
             $this->botStarted($userId);
             return;
         }
 
         if ((int)$contact->get('step_authorization') == TypeAutorization::START) {
             $phone = Form::sanitazePhone($data['message']['body']['text']);
-
             if (!Form::validatePhone($phone)) {
-                (new Messenger())->sendMessangeUser([
+                 $this->maxMessenger->sendMessangeUser([
                     'text' => "$phone Номер телефона не валидный"
                 ], $userId);
+            } elseif (!empty($phone) && (int)$phone === (int)$contact->phone) {
+                $this->auth($contact);
+                return;
             } else {
                 $code = rand(100000, 999999);
-
                 $contact = $contact->reContact($phone);
                 $contact->set('code', $code);
-                (new Messenger())->sendMessangeUser([
+                $this->maxMessenger->sendMessangeUser([
                     'text' => "Код отправлен на номер телефона $phone . Если хотите исправить номер телефона напишите " . implode(', ', $this->command)."."
                 ], $userId);
                 $contact->set('step_authorization', TypeAutorization::CODE);
                 $r = (new Sms())->send($phone, "Код авторизации бота в Mаx: $code");
                 self::dd("Responce SMSC: " . $r);
+                return;
             }
         }
 
         if ((int)$contact->get('step_authorization')  == TypeAutorization::CODE) {
             $code = trim($data['message']['body']['text']);
             if ($code == $contact->code) {
-                (new Messenger())->sendMessangeUser([
-                    'text' => "Вы успешно авторизовались теперь вы будете получать сообщения о записях и прочее от Альтамед+"
-                ], $userId);
-                $contact->set('step_authorization', TypeAutorization::AUTORIZATION);
+                $this->auth($contact);
             } else {
-                (new Messenger())->sendMessangeUser([
+                 $this->maxMessenger->sendMessangeUser([
                     'text' => "Неверный код."
                 ], $userId);
             }
         }
     }
 
+
     private static function dd($data): void
     {
         file_put_contents(__DIR__ . '/debug.txt', print_r($data, true) . "\n", FILE_APPEND);
+    }
+
+    private function auth($contact)
+    {
+        $this->maxMessenger->sendMessangeUser([
+            'text' => "Вы успешно авторизовались теперь вы будете получать сообщения о записях и прочее от Альтамед+"
+        ], $contact->max_user_id);
+        $contact->set('step_authorization', TypeAutorization::AUTORIZATION);
     }
 }
