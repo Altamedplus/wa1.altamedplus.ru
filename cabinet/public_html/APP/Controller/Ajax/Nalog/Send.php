c773789181582c9969e7e8cb74f994addb635205
Alexandr <alexandrlachin989@gmail.com>
2026-06-02 14:05:08 +0300

    update

--- cabinet/public_html/APP/Controller/Ajax/Nalog/Send.php
+++ cabinet/public_html/APP/Controller/Ajax/Nalog/Send.php
@@ -5,7 +5,10 @@ namespace APP\Controller\Ajax\Nalog;
 use APP\Controller\AjaxController;
 use APP\Enum\HistoryType;
 use APP\Enum\StatusMessage;
+use APP\Enum\TypeAutorization;
+use APP\Enum\TypeCannel;
 use APP\Form\Form;
+use APP\Model\Contact;
 use APP\Model\HistoryModel;
 use APP\Model\MessageModel;
 use APP\Model\NalogClinicModel;
@@ -22,22 +25,54 @@ class Send extends AjaxController
     {
         $id = (int)attr('id');
         $nalog  = new NalogModel($id);
-        $sample = new SampleModel(['name' => 'Готовность Налоговый вычет']);
+        $phone = $nalog->phone;
+        $sample = new SampleModel(['name' => 'Готовность налог 2026']);
         //$nalogClinic = new NalogClinicModel(['nalog_id' => $nalog->id, 'is_place' => 1]);
         $variable = [
             'fio' => [$nalog->taxpayer_fio],
             'nalogid' => [$nalog->id]
         ];
-        $data = $sample->complectWhatsApp($sample->id, $variable, [], null);
+        $contactMax = new Contact(['phone' => $phone, 'step_authorization' => TypeAutorization::AUTORIZATION]);
+        $contactTg = new Contact(['phone' => $phone, 'tg_step_auth' => TypeAutorization::AUTORIZATION]);
+        $isWA = true;
+        if ($contactMax->exist()) {
+            $dataMax = $sample->complectMax($sample->id, $variable, [], null);
+            $messangeId = (new MessageModel())->create([
+                'phone' => $nalog->phone,
+                'data_request' => json_encode($dataMax, JSON_UNESCAPED_UNICODE),
+                'user_id' => Auth::$profile['id'],
+                'sample_id' => $sample->id,
+                'status' => StatusMessage::QUEUE,
+                'type_send' =>  TypeCannel::MAX,
+            ]);
+            $isWA = false;
+        }
+        if ($contactTg->exist()) {
+            $dataTg = $sample->complectTelegram($sample->id, $variable, [], null);
+            $messangeId = (new MessageModel())->create([
+                'phone' => $nalog->phone,
+                'data_request' => json_encode($dataTg, JSON_UNESCAPED_UNICODE),
+                'user_id' => Auth::$profile['id'],
+                'sample_id' => $sample->id,
+                'status' => StatusMessage::QUEUE,
+                'type_send' =>  TypeCannel::TELEGRAM,
+            ]);
+            $isWA = false;
+        }
+
+        if ($isWA) {
+            $dataWa = $sample->complectWhatsApp($sample->id, $variable, [], null);
+            $messangeId = (new MessageModel())->create([
+                'phone' => $nalog->phone,
+                'data_request' => json_encode($dataWa, JSON_UNESCAPED_UNICODE),
+                'user_id' => Auth::$profile['id'],
+                'sample_id' => $sample->id,
+                'status' => StatusMessage::QUEUE,
+            ]);
+        }
         // $request = [];
         // $result =  (new WhatsApp())->sendWhatsapp($nalog->phone, $dataWa, $request);
-        $messangeId = (new MessageModel())->create([
-            'phone' => $nalog->phone,
-            'data_request' => json_encode($data, JSON_UNESCAPED_UNICODE),
-            'user_id' => Auth::$profile['id'],
-            'sample_id' => $sample->id,
-            'status' => StatusMessage::QUEUE,
-        ]);
+
 
         $nalog->is_send = 1;
         (new HistoryModel())->create([
--- cabinet/public_html/view/template/nalog/send_mail.php
+++ cabinet/public_html/view/template/nalog/send_mail.php
@@ -1,3 +1,33 @@
+<style>
+    .btn {
+        display: inline-block;
+        background-color: #007bff;
+        color: #ffffff;
+        font-size: 16px;
+        font-weight: 500;
+        font-family: inherit;
+        text-align: center;
+        text-decoration: none;
+        padding: 12px 24px;
+        border-radius: 6px;
+        border: none;
+        cursor: pointer;
+        transition: all 0.3s ease;
+        line-height: 1.5
+    }
+
+    .btn:hover {
+        background-color: #0056b3;
+        transform: translateY(-2px);
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    }
+
+    .btn:focus-visible {
+        outline: 2px solid #007bff;
+        outline-offset: 2px;
+    }
+</style>
+
 <div class="header_div">
     <img width="50" src="https://www.altamedplus.ru/upload/main/f04/f04d7dab6f33560da79ec27c78d63fe1.png" height="36" bxmailattachcid="1241"> <span class="header">Заказ справки для<br>налогового вычета</span>
 </div>
@@ -7,18 +37,20 @@
 </p>
 
 <p>
-    Вы успешно подали заявление на оформление справки для налогового вычета. Вашему заявлению присвоен порядковый номер <strong>№ <?=$id?></strong>.<br>
+    Вы успешно подали заявление на оформление справки для налогового вычета. Вашему заявлению присвоен порядковый номер <strong>№ <?= $id ?></strong>.<br>
     Cправка будет изготовлена в течение 10 дней после подачи Заявления. После подготовки справки мы сообщим Вам о ее готовности.<br>
-    <strong>Важно! Начиная с 2024 года справки формируются в электронном формате и отправляются в ФНС автоматически. За 2023 и 2022г. справки выдаются в бумажном формате.</strong></p>
+    <strong>Важно! Начиная с 2024 года справки формируются в электронном формате и отправляются в ФНС автоматически.
+        За 2023 справку вы можете запросить через чат мобильного приложения Альтамед+ или забрать в клинике.</strong>
+</p>
 <p style="color:#0086c1;"><strong>Обращаем ваше внимание, что услуги подолога не являются медицинскими (Письмо ФНС №ГД-4-3.6145@ от 04.04.2014) и расходы на них не включаются в справку о налоговом вычете.</strong></p>
 <p>
     <span class="form_data">
-        Дата подачи заявления: <?=date('d.m.Y')?><br>
-        Контактный телефон: <?=$phone?><br>
+        Дата подачи заявления: <?= date('d.m.Y') ?><br>
+        Контактный телефон: <?= $phone ?><br>
     </span>
 </p>
 <br>
-<p>Проверить статус заявления: <?=$statusUrl?> </p>
+<p>Проверить статус заявления: <a class="btn" href="<?= $statusUrl ?>" target="_blank"> Проверить статус заявления</p>
 
 <p>Не отвечайте на это письмо, оно сгенерировано автоматически. По всем вопросам вы можете обратиться по адресу <a href="mailto:nalog@altamed-plus.ru">nalog@altamed-plus.ru</a> или по телефону: <a href="tel:+74952129003">+7 (495) 212-90-03</a>.</p>
 <p>С уважением, сеть медицинских центров &laquo;Альтамед+&raquo;</p>
